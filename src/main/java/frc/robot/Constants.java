1d719c9dd28cf6c6a5822fe85bc626e30e033e7b
Na'Dyah Wynn <nadyah.wynn@richmond.edu>
2026-03-09 20:52:36 -0400

    initial commit to add swerve module

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,6 +15,9 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  
+  public static final double MAX_SPEED = Units.feetToMeters(14.5);
+
 
   /* The following classes are subjected to change based on CAN IDs */
 
@@ -48,6 +53,7 @@ public final class Constants {
   }
 
   public static class ShooterConstants {
+    // positve -> intake
     public static final int kShooterIntakePort = 13;
     public static final int kShooterLeftFlyWheelPort = 14;
     public static final int kShooterRightFlyWheelPort = 15;
