d575d0da795015343a1bc7b8ee4920b0e6737a3d
Gabriel McMahon <gabepmcmahon@gmail.com>
2026-03-19 20:12:54 -0400

    Spotless Applied

--- src/main/java/frc/robot/commands/AutoAimCommand.java
+++ src/main/java/frc/robot/commands/AutoAimCommand.java
@@ -4,19 +4,15 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.systems.field.AllianceFlipUtil;
 import frc.robot.systems.field.FieldConstants.Hub;
 import swervelib.SwerveInputStream;
 
+public class AutoAimCommand extends Command {
 
-public class AutoAimCommand extends Command
-{
-
-  private final SwerveSubsystem   swerveSubsystem;
+  private final SwerveSubsystem swerveSubsystem;
   private final SwerveInputStream swerveInputStream;
 
-  public AutoAimCommand(SwerveSubsystem swerveSubsystem)
-  {
+  public AutoAimCommand(SwerveSubsystem swerveSubsystem) {
     this.swerveSubsystem = swerveSubsystem;
     this.swerveInputStream = swerveSubsystem.baseStream.copy();
     // each subsystem used by the command must be passed into the
@@ -25,32 +21,27 @@ public class AutoAimCommand extends Command
   }
 
   @Override
-  public void initialize()
-  {
-    swerveInputStream.aim((new Pose2d(Hub.topCenterPoint.toTranslation2d(), Rotation2d.kZero)))
-                     .aimWhile(true)
-                     .aimHeadingOffset(Rotation2d.k180deg)
-                     .scaleTranslation(0.5);
-
+  public void initialize() {
+    swerveInputStream
+        .aim((new Pose2d(Hub.topCenterPoint.toTranslation2d(), Rotation2d.kZero)))
+        .aimWhile(true)
+        //                     .aimHeadingOffset(Rotation2d.k180deg)
+        .scaleTranslation(0.5);
   }
 
   @Override
-  public void execute()
-  {
+  public void execute() {
     swerveSubsystem.driveFieldOriented(swerveInputStream.get());
   }
 
   @Override
-  public boolean isFinished()
-  {
+  public boolean isFinished() {
     // TODO: Make this return true when this Command no longer needs to run execute()
     return false;
   }
 
   @Override
-  public void end(boolean interrupted)
-  {
-    swerveInputStream.aimWhile(false)
-                     .scaleTranslation(1);
+  public void end(boolean interrupted) {
+    swerveInputStream.aimWhile(false).scaleTranslation(1);
   }
 }
